bae1e70c382e6070da0a01e100cae52c2ae72c25
Fazenda-jpg <fazenda.j.p.s@hotmail.com>
2021-08-01 21:04:22 -0300

    Connect repository and service

--- src/main/java/com/example/workshopmongo/domain/User.java
+++ src/main/java/com/example/workshopmongo/domain/User.java
@@ -3,9 +3,14 @@ package com.example.workshopmongo.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable{
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+
+@Document(collection = "user")
+public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	@Id
 	private String Id;
 	private String name;
 	private String email;
@@ -15,6 +20,7 @@ public class User implements Serializable{
 	}
 
 	public User(String id, String name, String email) {
+		super();
 		Id = id;
 		this.name = name;
 		this.email = email;
