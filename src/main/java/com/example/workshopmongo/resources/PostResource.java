c360f98c3436f762b17cce14668d198a159dc231
Fazenda-jpg <fazenda.j.p.s@hotmail.com>
2021-08-02 20:21:08 -0300

    full search

--- src/main/java/com/example/workshopmongo/resources/PostResource.java
+++ src/main/java/com/example/workshopmongo/resources/PostResource.java
@@ -1,6 +1,6 @@
 package com.example.workshopmongo.resources;
 
-import java.net.URI;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,6 +33,17 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
 		text = URL.decoParam(text);
 		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+
+	@RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		text = URL.decoParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = postService.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 
